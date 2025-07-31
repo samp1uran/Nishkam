b6c66f4ea2d0c4273cb75e041b1ff0a357667527
Harshit bhandari <hbhandari0005@gmail.com>
2025-07-31 17:41:03 +0530

    Fixed toast container

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,25 +2,25 @@ import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 import Chat from "./Chat";
-import { ToastContainer } from 'react-toastify'
+import { ToastContainer } from "react-toastify";
 export default function Layout() {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
       <main className="pt-20 p-4 flex-grow">
-        <Chat/>
+        <Chat />
         <Outlet />
       </main>
       <Footer />
       <ToastContainer
-          position="top-right"
-          autoClose={3000}
-          hideProgressBar={false}
-          toastClassName="toast-class"
-          closeOnClick
-          pauseOnHover
-          draggable
-        />
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        closeOnClick
+        pauseOnHover
+        draggable
+        limit={3}
+      />
     </div>
   );
 }
